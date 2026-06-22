b8d6ad9c7768b5af3664553602425de2ae90140f
USERcristhian01 <141697738+USERcristhian01@users.noreply.github.com>
2026-06-22 15:18:39 -0500

    chore: remove all project files and source code

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PersonController;
-
-Route::get('/dni/{dni}', [PersonController::class, 'searchByDni']);
-Route::get('/ruc/{ruc}', [PersonController::class, 'searchByRuc']);
