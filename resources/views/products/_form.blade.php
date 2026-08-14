86310669703bfc2ff795d3893f7d27335fa8427d
FrankOcrospomaUgaz <anthony.2005.rap@gmail.com>
2026-08-13 19:52:50 -0500

    feat: add product management form with dynamic branch and inventory configurations

--- resources/views/products/_form.blade.php
+++ resources/views/products/_form.blade.php
@@ -301,10 +301,9 @@
             <div>
                 <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">Codigo <span
                         class="text-red-500">*</span></label>
-                <input type="text" name="code" x-model="code" :readonly="!isEdit" required
+                <input type="text" name="code" x-model="code" required
                     placeholder="Ingrese el codigo"
-                    class="dark:bg-dark-900 shadow-theme-xs focus:border-[#124731] focus:ring-[#124731]/10 dark:focus:border-[#124731] h-11 w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
-                    :class="!isEdit ? 'bg-gray-100 cursor-not-allowed text-gray-500 dark:bg-gray-800' : 'bg-transparent'" />
+                    class="dark:bg-dark-900 shadow-theme-xs focus:border-[#124731] focus:ring-[#124731]/10 dark:focus:border-[#124731] h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30" />
                 @error('code')
                     <p class="mt-1 text-xs text-red-600 dark:text-red-400">{{ $message }}</p>
                 @enderror
